4de37eb9dbef5d21cdbf75434dd0638b521f4a1f
luodan0709 <luodan0709@live.cn>
2017-03-31 15:22:30 +0800

    删除废弃的插件

--- luci/applications/luci-app-youku1/files/usr/lib/lua/luci/controller/youku.lua
+++
@@ -1,9 +0,0 @@
-module("luci.controller.youku", package.seeall)
-
-function index()
-	if not nixio.fs.access("/etc/config/youku") then
-		return
-	end
-
-	entry({"admin", "services", "youku"}, cbi("youku"), _("优酷路由宝")).dependent = true
-end
